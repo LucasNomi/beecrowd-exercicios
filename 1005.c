7c18fec5912efa045ce63a757b53b06303b1b490
LucasNomi <lucas04.dev@gmail.com>
2023-07-05 16:25:19 -0300

    feat: exercicio 1005.c resolvido

---
+++ 1005.c
@@ -0,0 +1,11 @@
+#import <stdio.h>
+
+int main(void) {
+  double A, B;
+
+  scanf("%lf %lf", &A, &B);
+
+  double MEDIA = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+  printf("MEDIA = %.5lf\n", MEDIA);
+  return 0;
+}
